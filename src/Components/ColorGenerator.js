a2b95f06adc3bba0d3b8ff2c08c8d3dc81b61bf7
Fuad_Hashimov <fuadheshimzade@gmail.com>
2022-02-13 20:42:08 +0400

    Initial Commit

---
+++ src/Components/ColorGenerator.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import ColorsUI from "./ColorsUI";
+
+class ColorGenerator extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  genaratorColor = () => {
+    const str = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      let index = Math.floor(Math.random() * str.length);
+      color += str[index];
+    }
+    return color;
+  };
+  colorsArr = () => {
+    const arr = [];
+    for (let i = 0; i < this.genaratorColor().length; i++) {
+      arr.push(this.genaratorColor());
+    }
+
+    return arr;
+  };
+
+  handleClick = () => {
+    this.colorsArr();
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="rows">
+          <ColorsUI colors={this.colorsArr()} />
+          <ColorsUI colors={this.colorsArr()} />
+          <ColorsUI colors={this.colorsArr()} />
+          <ColorsUI colors={this.colorsArr()} />
+        </div>
+      </div>
+    );
+  }
+}
+export default ColorGenerator;
---
+++ src/Components/ColorsUI.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class ColorsUI extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { colors } = this.props;
+    return (
+      <>
+        {colors.map((m, idx) => {
+          return (
+            <div
+            className="box-color"
+              key={idx}
+              style={{ backgroundColor: m}}
+            >
+              {m}
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+}
+export default ColorsUI;
